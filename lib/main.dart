af95f789af7cde2f3ff200114604a6c4e8caf100
Raphael Busquet <73319921+raphaelbusquet@users.noreply.github.com>
2023-08-06 22:01:47 +0100

    ...

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
-
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+ 
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
